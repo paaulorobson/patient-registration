33b4f4fe78f0a58f7b452108db8e9af0a4ad597c
Paulo Robson <paulo.robson@contabilizei.com.br>
2023-04-18 11:19:00 -0300

    feat: estruturando projeto

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import VueTheMask from "vue-the-mask";
+import "./assets/css/app.css";
 
+Vue.use(VueTheMask);
 Vue.config.productionTip = false;
 
 new Vue({
