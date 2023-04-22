e75bd9ab5bb864ad08d284d8952da9c8b8eba7dc
Paulo Robson <paulo.robson@contabilizei.com.br>
2023-04-22 15:50:51 -0300

    feat: ajustando rota

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -7,11 +7,11 @@
     <img class="p-4" src="../assets/logo-om30.png" alt="logo om30" v-show="!dataOpenSidebar" />
     <div class="h-[calc(100vh-50px) py-[50px]">
       <router-link
-        to="/pacientes"
+        to="/"
         :class="{
           'inline-flex relative items-center py-[10px] pl-2 w-full text-sm sm:text-lg font-medium border-gray-200 hover:bg-white hover:text-gray-800 transition duration-400 ease-in-out': true,
           'inline-flex bg-white relative items-center py-[10px] pl-2 w-full text-sm sm:text-lg font-medium border-gray-200 hover:bg-white hover:text-gray-800 transition duration-400 ease-in-out':
-            $route.path === '/pacientes',
+            $route.path === '/',
         }"
       >
         <img class="w-[25px] h-[25px] mr-2" src="../assets/medical.svg" alt="icon prontuario" />
--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,7 @@ Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/pacientes',
+    path: '/',
     name: 'pacientes',
     component: Pacientes,
   },
